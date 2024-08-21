2abdabdb0ac4a73bbbb99793677b44f9378db98f
chandrakanthrck <chandrakanthrajesh@Chandrakanths-MacBook-Pro-2.local>
2024-08-21 13:15:20 -0700

    adding discovery client to Hotel Service

--- HotelService/src/main/java/com/lcwd/hotel/HotelService/HotelServiceApplication.java
+++ HotelService/src/main/java/com/lcwd/hotel/HotelService/HotelServiceApplication.java
@@ -2,7 +2,9 @@ package com.lcwd.hotel.HotelService;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class HotelServiceApplication {
 
