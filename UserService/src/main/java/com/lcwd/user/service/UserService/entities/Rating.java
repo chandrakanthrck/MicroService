7c7b980e367614d1a1bce3d0ca6425faca301e37
chandrakanthrck <chandrakanthrajesh@Chandrakanths-MacBook-Pro-2.local>
2024-08-21 14:28:51 -0700

    retreiving ratings of users and mapping it to the hotel associated to it using rest templates

--- UserService/src/main/java/com/lcwd/user/service/UserService/entities/Rating.java
+++ UserService/src/main/java/com/lcwd/user/service/UserService/entities/Rating.java
@@ -16,4 +16,6 @@ public class Rating {
     private String hotelId;
     private int rating;
     private String feedback;
+    //from other service
+    private Hotel hotel;
 }
--- UserService/src/main/java/com/lcwd/user/service/UserService/services/impl/UserServiceImpl.java
+++ UserService/src/main/java/com/lcwd/user/service/UserService/services/impl/UserServiceImpl.java
@@ -1,5 +1,6 @@
 package com.lcwd.user.service.UserService.services.impl;
 
+import com.lcwd.user.service.UserService.entities.Hotel;
 import com.lcwd.user.service.UserService.entities.Rating;
 import com.lcwd.user.service.UserService.entities.User;
 import com.lcwd.user.service.UserService.exceptions.ResourceNotFoundException;
@@ -7,6 +8,7 @@ import com.lcwd.user.service.UserService.repositories.UserRepository;
 import com.lcwd.user.service.UserService.services.UserService;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 import org.slf4j.Logger;
@@ -15,6 +17,7 @@ import org.slf4j.Logger;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 @Service
 public class UserServiceImpl implements UserService {
@@ -52,9 +55,22 @@ public class UserServiceImpl implements UserService {
         //rest template implementation
         //restTemplate.getForObject -> sendsa HTTP GET request to the specified URL and converts it into an instance of the specified class
         //ArrayList -> to desrialize it to ArrayList of Rating object
+        //but the drawback with this is that the route is fine, but we want to make the domain address dynamic
         ArrayList<Rating> ratingsOfUser = restTemplate.getForObject("http://localhost:8083/ratings/users/"+user.getUserId(), ArrayList.class);
         logger.info("{}",ratingsOfUser);
-        user.setRatings(ratingsOfUser);
+
+        ratingsOfUser.stream().map(rating -> {
+            //api call to hotel service to get the hotel
+            //http://localhost:8081/users/2be72313-8c76-4b07-8100-4e904726223a
+            ResponseEntity<Hotel> forEntity = restTemplate.getForEntity("http://localhost:8082/hotels/"+rating.getHotelId(), Hotel.class);
+            Hotel hotel = forEntity.getBody();
+            logger.info("Response status code: {} ",forEntity.getStatusCode());
+            //set the hotel rating
+            rating.setHotel(hotel);
+            //return to rating
+            return rating;
+        }).collect(Collectors.toList())
+;        user.setRatings(ratingsOfUser);
         return user;
     }
 }
