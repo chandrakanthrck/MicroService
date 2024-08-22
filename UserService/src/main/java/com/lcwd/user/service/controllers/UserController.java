3ecaa4681b3a1e25262d6730ad56634c9fc19e0c
chandrakanthrck <chandrakanthrajesh@Chandrakanths-MacBook-Pro-2.local>
2024-08-21 23:34:02 -0700

    adding health monitor and implementing fallback methods for UserService

--- UserService/src/main/java/com/lcwd/user/service/controllers/UserController.java
+++ UserService/src/main/java/com/lcwd/user/service/controllers/UserController.java
@@ -2,6 +2,9 @@ package com.lcwd.user.service.controllers;
 
 import com.lcwd.user.service.UserService.entities.User;
 import com.lcwd.user.service.UserService.services.UserService;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,36 +15,39 @@ import java.util.List;
 @RestController
 @RequestMapping("/users")
 public class UserController {
+
+    private static final Logger logger = LoggerFactory.getLogger(UserController.class);
+
     @Autowired
     private UserService userService;
 
-    //create
-    //sending response after creating
-    //using @RequestBody, the body get deserialized
-    //when you use post mapping on /users this method runs
-    //using post mapping it creates
+    // Create a new user
     @PutMapping("create")
-    public ResponseEntity<User> createUser(@RequestBody User user){
-        //to save this data, we must use saveUser
+    public ResponseEntity<User> createUser(@RequestBody User user) {
         User user1 = userService.saveUser(user);
         return ResponseEntity.status(HttpStatus.CREATED).body(user1);
     }
 
-    //single user get
-    //using get mapping it returns
+    // Get a single user by ID
     @GetMapping("/{userId}")
-    //using @PathVariable, the userId that comes in getmapping will be used
-    //as parameter inside getSingleUser
-    public ResponseEntity<User> getSingleUser(@PathVariable String userId){
-        //getting user
-       User user =  userService.getUser(userId);
-       //.ok() for status 200 ok
-       return ResponseEntity.ok(user);
+    @CircuitBreaker(name = "ratingHotelBreaker", fallbackMethod = "ratingHotelFallback")
+    public ResponseEntity<User> getSingleUser(@PathVariable String userId) {
+        User user = userService.getUser(userId);
+        return ResponseEntity.ok(user);
     }
 
-    //all user get
+    // Fallback method for Circuit Breaker
+    public ResponseEntity<User> ratingHotelFallback(String userId, Exception ex) {
+        logger.info("Fallback is executed because service is down: {}", ex.getMessage());
+        User fallbackUser = new User(); // You can customize this as per your requirement
+        fallbackUser.setUserId(userId);
+        fallbackUser.setName("Fallback User");
+        return new ResponseEntity<>(fallbackUser, HttpStatus.OK);
+    }
+
+    // Get all users
     @GetMapping
-    public ResponseEntity<List<User>> getAllUser(){
+    public ResponseEntity<List<User>> getAllUser() {
         List<User> allUser = userService.getAllUser();
         return ResponseEntity.ok(allUser);
     }
